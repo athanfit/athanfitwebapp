3695453250d9b0e5179bc4da932aea7bcba97dd5
jelco <jelco4260@gmail.com>
2022-10-25 00:16:05 +0200

    password reset update DB works

--- password/resetProcess.php
+++ password/resetProcess.php
@@ -14,7 +14,40 @@ if (isset($_SERVER["HTTP_REFERER"])     && $_SERVER["HTTP_REFERER"] == "https://
             !empty($_POST["csrfToken"])) 
         {
             if (isset($_SESSION["token"]) && $_SESSION["token"] == $_POST["csrfToken"]) {
-                echo "all good";
+                $sql = "SELECT UserID, `Hash`, `Date` FROM `Password` WHERE UserID = '$userID' AND `Hash` = '$hash'";
+                $result=$mysqli->query($sql); 
+                if($result){ 
+                    if ($result -> num_rows > 0) {
+                    $fetchUserData = $result->fetch_assoc(); 
+                    $date = $fetchUserData['Date'];
+                    if ($date == $today){
+                        $TextPassword = $_POST['Password'];
+                        $saltPassword = $TextPassword . "PannenkoekenStraat";
+                        $Password = hash('sha256', $saltPassword);
+                        $sql = "UPDATE Users SET UserPassword='$Password' WHERE UserID='$userID'";
+                        if ($mysqli->query($sql) === TRUE) {
+                            echo "Record updated successfully";
+                        } else {
+                            echo "Error updating record: " . $mysqli->error;
+                        }
+                        $mysqli->close();
+                        ?>
+                        <div class="container">
+                            <div class="col-sm-7 smallcard">
+                                <div class="card">
+                                <h5 class="card-header">Password reset</h5>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Password has been reset</h5>
+                                        <p class="card-text">You can now login with your new password.</p>
+                                        <a href="../" class="card-link">Login on homepage</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <?php
+                        }
+                    }
+                }
             } else {
                 ?><script>console.log("Token is not right.");</script><?php
             }
