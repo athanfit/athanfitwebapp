e20d9268696f4db90c8b9fcb47a3e0ff8bed171d
jelco <jelco4260@gmail.com>
2022-10-25 14:39:47 +0200

    personal info on account page

--- Account/index.php
+++ Account/index.php
@@ -1,17 +1,35 @@
 <?php
 $title = "Account";
 require '../includes/check.php';
+require '../includes/config.php';
 include '../includes/head.php';
 include '../includes/navbar.php';
-require '../includes/config.php';
+$ID = $_SESSION['ID'];
+$sql = "SELECT * FROM Users WHERE UserID = '$ID'";
+$result=$mysqli->query($sql); 
+$fetchUserData = $result->fetch_assoc(); 
+$Firstname = $fetchUserData['UserFirstname'];
+$Lastname = $fetchUserData['UserLastname'];
+$Email = $fetchUserData['UserEmail'];
+$Verified = $fetchUserData['verified'];
 ?>
 <div class="container">
     <div class="col-sm-7 smallcard">
         <div class="card">
             <h5 class="card-header">Account page</h5>
             <div class="card-body">
-            <h5 class="card-title">Account of <?= $_SESSION['Firstname'] ?></h5>
+            <h5 class="card-title">Account of <?= $Firstname . " " . $Lastname ?></h5>
             <p class="card-text">Find here the info of your account and to update info of your account.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">Fristname: <?= $Firstname ?></li>
+                    <li class="list-group-item">Lastname: <?= $Lastname ?></li>
+                    <li class="list-group-item">Email: <?= $Email ?></li>
+                    <li class="list-group-item">ID: <?= $ID ?></li>
+                </ul>
             </div>
         </div>
     </div>
