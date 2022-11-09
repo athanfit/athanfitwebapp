99d910b2b61760167c21d57221038c79f42a4e08
Jelco <56719136+JGHeringa@users.noreply.github.com>
2022-11-09 12:06:43 +0100

    new program form and process

--- js/newInput.js
+++ js/newInput.js
@@ -4,7 +4,7 @@ var dynamicInput = [];
 function addInput(){
     var newdiv = document.createElement('div');
     newdiv.id = dynamicInput[counter];
-    newdiv.innerHTML = "Exersice" + "<div class='form-group'><input type='text' class='form-control' name='Exersice' id='Exersice'><button type='button' class='btn btn-outline-secondary smallBtn' onClick='removeInput("+dynamicInput[counter]+");'> - </button></div>";
+    newdiv.innerHTML = "Exersice" + "<div class='form-group'><input type='text' class='form-control' name='Exersice"+counter+"' id='Exersice'><button type='button' class='btn btn-outline-secondary smallBtn' onClick='removeInput("+dynamicInput[counter]+");'> - </button></div>";
     document.getElementById('exersices').appendChild(newdiv);
     counter++;
 }
--- program/nieuwProcess.php
+++ program/nieuwProcess.php
@@ -4,13 +4,45 @@ $title = "Aanleden";
 include '../includes/config.php';
 include '../includes/head.php';
 include '../includes/navbar.php';
+$userID = $_SESSION["ID"];
+$exercise = array();
 $today = date("Y-m-d");
 if (isset($_SERVER["HTTP_REFERER"])     && $_SERVER["HTTP_REFERER"] == "https://train.4260.nl/program/nieuw.php")
 {
     if (!empty($_POST['csrfToken'])){
         if (isset($_SESSION["token"]) && $_SESSION["token"] == $_POST["csrfToken"]) 
         {
-            echo "werkt";
+            $title = $_POST['title'];
+            $description = $_POST['description'];
+            $data = $_POST;
+            array_shift($data);
+            array_shift($data);
+            array_pop($data);
+            echo $title."<br>";
+            echo $description."<br>";
+            foreach ($data as $value) {
+                array_push($exercise, $value);
+            }
+            print_r($exercise)."<br>";
+            $dbExercise = serialize($exercise);
+            do {
+                $permitted_chars = '1234567890abcdeABCDE1234567890';
+                $ID = substr(str_shuffle($permitted_chars), 0, 12);
+                $result = mysqli_query($mysqli, "SELECT ID FROM `Programs` WHERE ID = '$ID'");
+                $rows = mysqli_num_rows($result);
+            }
+            while($rows > 1);
+            $sql = "INSERT INTO `Programs` (ID, `Title`, `Description`, `Program`, `Date`, `UserID`) VALUES  (?, ?, ?, ?, ?, ?)";
+            if ($stmt = $mysqli->prepare($sql)) {
+                $stmt->bind_param('ssssss', $ID, $title, $description, $dbExercise, $today, $userID);
+                if ($stmt->execute()) {
+                    ?><script>console.log("Done, data to DB");</script><?php
+                } else {
+                    echo "Something went wrong!";
+                }
+                } else {
+                    echo "zit een fout in de query: " . $mysqli->error;
+                }
         } else {
             ?><script>console.log("token not right");</script><?php
         }
