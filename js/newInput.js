02fd4749702607cce06b062bfcb9fae016a46d86
Jelco <56719136+JGHeringa@users.noreply.github.com>
2022-11-15 21:15:56 +0100

    new input fixed, no undined

--- js/newInput.js
+++ js/newInput.js
@@ -1,12 +1,15 @@
 var counter = 1;
 var dynamicInput = [];
-var value = "";
 
 function addInput(value){
     var newdiv = document.createElement('div');
-    newdiv.id = dynamicInput[counter];
-    const exercise = value;
-    newdiv.innerHTML = "Exersice" + "<div class='form-group'><input value='"+exercise+"' type='text' class='form-control' name='Exersice"+counter+"' id='"+counter+"'><button type='button' class='btn btn-outline-secondary smallBtn' onClick='removeInput("+dynamicInput[counter]+");'> - </button></div>";
+    newdiv.id = counter;
+    if (value){
+        var exercise = value
+    } else {
+        var exercise = "";
+    }
+    newdiv.innerHTML = "Exersice" + "<div class='form-group'><input value='"+exercise+"' type='text' class='form-control' name='Exersice"+counter+"' id='"+counter+"'><button type='button' class='btn btn-outline-secondary smallBtn' onClick='removeInput("+counter+");'> - </button></div>";
     document.getElementById('exersices').appendChild(newdiv);
     counter++;
 }
@@ -14,4 +17,3 @@ function removeInput(id){
     var elem = document.getElementById(id);
     return elem.parentNode.removeChild(elem);
 }
-// (counter + 1) + 
