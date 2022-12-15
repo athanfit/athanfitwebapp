465a0611e71724a1303c33237d6affd75f13c56a
Jelco Heringa <contact@jelcoheringa.nl>
2022-12-15 13:47:58 +0100

    changed text in edit button and begin for link to workout

--- program/edit.php
+++ program/edit.php
@@ -57,7 +57,7 @@ if (isset($_SESSION['ID'])   &&
                         }
                         ?>
                         <input type="hidden" name="csrfToken" value="<?= $Token ?>">
-                        <button type="submit" class="btn btn-primary btn-block SubmitBtn">Submit</button>
+                        <button type="submit" class="btn btn-primary btn-block SubmitBtn">Save changes</button>
                         <button type="button" class="btn btn-danger extraBtn SubmitBtn" onClick="myFunction()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg></button>
                         <button type="button" class="btn btn-secondary extraBtn SubmitBtn" onClick="addInput()">Add exersice</button>
                     </form>
--- program/index.php
+++ program/index.php
@@ -16,7 +16,7 @@ if (isset($_SESSION['ID'])   &&
             <div class="col-sm-4">
                 <div class="card">
                     <div class="card-body">
-                        <h5>Programs</h5>
+                        <h1>Programs</h1>
                     </div>
                 </div>
                 <div class="d-grid gap-2">
@@ -34,20 +34,19 @@ if (isset($_SESSION['ID'])   &&
                     $description = $row['Description'];
                     $data = $row['Program'];
                     $program = unserialize($data);
-
                     ?>
                     <div class="card">
                         <div class="card-body">
                         <a type="button" href="edit.php?ID=<?= $id ?>" class="btn btn-outline-primary SubmitBtn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16"><path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/></svg></a>
                             <h5><?= $title ?></h5>
                             <p><?= $description ?></p>
-                            <ul class="list-group">
+                            <div class="list-group">
                                 <?php
                                 foreach ($program as $value) {
-                                    ?><li class="list-group-item"><?= $value ?></li><?php
+                                    ?><a href="#" class="list-group-item list-group-item-action"><?= $value ?></a><?php
                                 }
                                 ?>
-                            </ul>
+                            </div>
                             <a type="button" href="edit.php?ID=<?= $id ?>" class="btn btn-outline-primary SubmitBtn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg></a>
                         </div>
                     </div>
